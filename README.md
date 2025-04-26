e27188dc70c3f8d2b578618158083f5071ba594d
Peotr-B <peotr60@mail.ru>
2025-04-26 10:42:50 +0300

    Nachalo
    Создание проекта

---
+++ README.md
@@ -0,0 +1,12 @@
+# Scriptor42
+ 
+ 26апр5
+ @Scriptor42
+ Scriptor42@list.ru
+
+Проект "Подключение датчика температуры DS18B20 к NodeMCU ESP8266" с использованием облачного сервиса:
+https://microkontroller.ru/esp8266-projects/podklyuchenie-datchika-temperatury-ds18b20-k-esp8266/
+
+Дисклеймер
+Данный проект является частью подготовки к созданию системы Умный Дом (IoT)
+
---
+++ TemoCloud.ino
@@ -0,0 +1,34 @@
+/* 26апр25
+https://microkontroller.ru/esp8266-projects/podklyuchenie-datchika-temperatury-ds18b20-k-esp8266/
+Подключение датчика температуры DS18B20 к NodeMCU ESP8266
+*/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 4;     
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  // Start the Serial Monitor
+  Serial.begin(115200);
+  // Start the DS18B20 sensor
+  sensors.begin();
+}
+
+void loop() {
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+  float temperatureF = sensors.getTempFByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+  Serial.print(temperatureF);
+  Serial.println("ºF");
+  delay(5000);
+}
